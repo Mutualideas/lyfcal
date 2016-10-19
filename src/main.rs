63898a038bd841ff2efc147a1b2c61322211a27c
Mutualideas <jason.lu.nz@hotmail.com>
2016-10-20 00:45:08 +1100

    added opacity sliders

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ extern crate find_folder;
 
 use piston_window::{EventLoop, OpenGL, PistonWindow, UpdateEvent, WindowSettings};
 
+
+struct App {
+    past_opacity: f32,
+    future_opacity: f32,
+}
+
+
 fn main() {
 
 
@@ -28,6 +35,10 @@ fn main() {
 
     window.set_ups(60);
 
+    let mut app = App {
+        past_opacity: 0.0,
+        future_opacity: 0.7,
+    };
 
     //////////////////////////
     ///// User Interface /////
@@ -66,7 +77,7 @@ fn main() {
 
         event.update(|_| {
             let mut ui = ui.set_widgets();
-            gui(&mut ui, &ids);
+            gui(&mut ui, &ids, &mut app);
         });
 
         window.draw_2d(&event, |c, g| {
@@ -92,21 +103,67 @@ fn main() {
 widget_ids!{
     struct Ids {
         background,
-        title
+        title,
+        past_opacity,
+        past_opacity_label,
+        future_opacity,
+        future_opacity_label,
     } 
 }
 
-fn gui(ui: &mut conrod::UiCell, ids: &Ids) {
-    use conrod::{widget, Colorable, Positionable, Widget};
+fn gui(ui: &mut conrod::UiCell, ids: &Ids, app: &mut App) {
+    use conrod::{widget, Borderable, Colorable, Positionable, Sizeable, Widget};
 
     widget::Canvas::new()
         .color(conrod::color::WHITE)
         .set(ids.background, ui);
+
     widget::Text::new("lyfcal")
         .font_size(46)
         .color(conrod::color::LIGHT_CHARCOAL)
         .mid_top_with_margin_on(ids.background, 24.0)
         .set(ids.title, ui);
+
+   //Sliders
+   
+   
+    const SLIDER_H: f64 = 24.0;
+
+    fn opacity_slider(
+        opacity: &mut f32,
+        label: &str,
+        slider_id: widget::Id,
+        label_id: widget::Id,
+        background: widget::Id,
+        ui: &mut conrod::UiCell,
+    ) {
+
+        for new_opacity in widget::Slider::new(*opacity, 0.0, 1.0)
+            .padded_w_of(background, 24.0)
+            .middle_of(background)
+            .down(16.0)
+            .h(SLIDER_H)
+            .color(conrod::color::LIGHT_CHARCOAL)
+            .border(0.0)
+            .set(slider_id, ui)
+        {
+            *opacity = new_opacity;
+        }
+        
+        let font_size = (SLIDER_H / 2.0) as u32;
+        let label = format!("{} opacity {}%", label, (*opacity * 100.0).trunc());
+        widget::Text::new(&label)
+            .font_size(font_size)
+            .color(conrod::color::WHITE)
+            .mid_top_with_margin_on(slider_id, -6.0)
+            .graphics_for(slider_id)
+            .set(label_id, ui);
+    }
+
+    opacity_slider(&mut app.past_opacity, "past", ids.past_opacity, ids.past_opacity_label, ids.background, ui);
+
+    opacity_slider(&mut app.future_opacity, "future", ids.future_opacity, ids.future_opacity_label, ids.background, ui);
+  
 }
 
 
