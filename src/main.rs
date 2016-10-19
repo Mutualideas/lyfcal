42b1b04b5e346ccd1e61df728102fc445b33458a
Mutualideas <jason.lu.nz@hotmail.com>
2016-10-20 02:17:36 +1100

    added life expectancy slider and changed title font

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use piston_window::{EventLoop, OpenGL, PistonWindow, UpdateEvent, WindowSettings
 struct App {
     past_opacity: f32,
     future_opacity: f32,
+    life_expectancy: f32,
 }
 
 
@@ -38,6 +39,7 @@ fn main() {
     let mut app = App {
         past_opacity: 0.0,
         future_opacity: 0.7,
+        life_expectancy: 80.0,
     };
 
     //////////////////////////
@@ -51,7 +53,10 @@ fn main() {
     // Add a `Font` to the `Ui`'s `font::Map` from file.
     let assets = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
     let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
-    ui.fonts.insert_from_file(font_path).unwrap();
+    let notosans_id = ui.fonts.insert_from_file(font_path).unwrap();
+    ui.theme.font_id = Some(notosans_id);
+    let font_path = assets.join("fonts/bitstream-vera-sans-mono/Bitstream Vera Sans Mono Roman.ttf");
+    let bitstream_id = ui.fonts.insert_from_file(font_path).unwrap();
 
     // Create a texture to use for efficiently caching text on the GPU.
     let mut text_texture_cache =
@@ -77,7 +82,7 @@ fn main() {
 
         event.update(|_| {
             let mut ui = ui.set_widgets();
-            gui(&mut ui, &ids, &mut app);
+            gui(&mut ui, &ids, &mut app, bitstream_id);
         });
 
         window.draw_2d(&event, |c, g| {
@@ -108,10 +113,12 @@ widget_ids!{
         past_opacity_label,
         future_opacity,
         future_opacity_label,
+        life_expectancy,
+        life_expectancy_label,
     } 
 }
 
-fn gui(ui: &mut conrod::UiCell, ids: &Ids, app: &mut App) {
+fn gui(ui: &mut conrod::UiCell, ids: &Ids, app: &mut App, title_font: conrod::text::font::Id) {
     use conrod::{widget, Borderable, Colorable, Positionable, Sizeable, Widget};
 
     widget::Canvas::new()
@@ -119,19 +126,23 @@ fn gui(ui: &mut conrod::UiCell, ids: &Ids, app: &mut App) {
         .color(conrod::color::WHITE)
         .set(ids.background, ui);
 
-    widget::Text::new("lyfcal")
-        .font_size(46)
+    let mut title = widget::Text::new("lyfcal");
+    title.style.font_id = Some(Some(title_font));
+    title
+        .font_size(38)
         .color(conrod::color::LIGHT_CHARCOAL)
         .mid_top_with_margin_on(ids.background, 24.0)
         .set(ids.title, ui);
 
-   //Sliders
-   
+   //Sliders//
    
     const SLIDER_H: f64 = 24.0;
 
-    fn opacity_slider(
-        opacity: &mut f32,
+    fn slider(
+        value: &mut f32,
+        min: f32,
+        max: f32,
+        down: f64,
         label: &str,
         slider_id: widget::Id,
         label_id: widget::Id,
@@ -139,20 +150,19 @@ fn gui(ui: &mut conrod::UiCell, ids: &Ids, app: &mut App) {
         ui: &mut conrod::UiCell,
     ) {
 
-        for new_opacity in widget::Slider::new(*opacity, 0.0, 1.0)
+        for new_value in widget::Slider::new(*value, min, max)
             .padded_w_of(background, 24.0)
             .middle_of(background)
-            .down(16.0)
+            .down(down)
             .h(SLIDER_H)
             .color(conrod::color::LIGHT_CHARCOAL)
             .border(0.0)
             .set(slider_id, ui)
         {
-            *opacity = new_opacity;
+            *value = new_value;
         }
         
         let font_size = (SLIDER_H / 2.0) as u32;
-        let label = format!("{} opacity {}%", label, (*opacity * 100.0).trunc());
         widget::Text::new(&label)
             .font_size(font_size)
             .color(conrod::color::WHITE)
@@ -161,9 +171,14 @@ fn gui(ui: &mut conrod::UiCell, ids: &Ids, app: &mut App) {
             .set(label_id, ui);
     }
 
-    opacity_slider(&mut app.past_opacity, "past", ids.past_opacity, ids.past_opacity_label, ids.background, ui);
+    let label = format!("life expectancy {} years", app.life_expectancy.trunc());
+    slider(&mut app.life_expectancy, 5.0, 100.0, 32.0, &label, ids.life_expectancy, ids.life_expectancy_label, ids.background, ui);
 
-    opacity_slider(&mut app.future_opacity, "future", ids.future_opacity, ids.future_opacity_label, ids.background, ui);
+    let label = format!("past opacity {}%", (app.past_opacity * 100.0).trunc());
+    slider(&mut app.past_opacity, 0.0, 1.0, 16.0, &label, ids.past_opacity, ids.past_opacity_label, ids.background, ui);
+
+    let label = format!("future opacity {}%", (app.future_opacity * 100.0).trunc());
+    slider(&mut app.future_opacity, 0.0, 1.0, 16.0, &label, ids.future_opacity, ids.future_opacity_label, ids.background, ui);
   
 }
 
