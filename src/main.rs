177b2f3fb0a96b8322bbeab37ef3a2bcfb491c28
Mutualideas <jason.lu.nz@hotmail.com>
2024-06-29 23:47:02 +1000

    Added logo icon

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+// hide console window on Windows in release
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use egui::IconData;
+
 mod config;
 mod draw;
 mod gui;
 
 fn main() -> Result<(), eframe::Error> {
+    let icon = image::open("src/assets/icon.png")
+        .expect("Failed to open icon path")
+        .to_rgba8();
+    let (icon_width, icon_height) = icon.dimensions();
+    let icon_data = IconData {
+        rgba: icon.into_raw(),
+        width: icon_width,
+        height: icon_height,
+    };
+
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_title("lyfcal")
             .with_maximize_button(false)
-            //.with_icon(icon)
+            .with_icon(icon_data)
             .with_inner_size([345.0, 470.0])
             //.with_transparent(true)
             .with_resizable(false)
